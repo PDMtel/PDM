b63bd8487b5b6b39865e1a180a789d0278efa46b
PDMtel <71953068+PDMtel@users.noreply.github.com>
2020-09-26 21:07:44 -0700

    Update main.go

--- main.go
+++ main.go
@@ -1,35 +1,49 @@
 package main
 
 import (
-    "log"
-    "os"
+	"log"
+	"os"
 
-    tb "gopkg.in/tucnak/telebot.v2"
+	"github.com/joho/godotenv"
+	"github.com/yanzay/tbot/v2"
 )
 
+type score struct {
+	wins, draws, losses uint
+}
+
+type application struct {
+	client *tbot.Client
+	score
+}
+
+var (
+	app     application
+	bot     *tbot.Server
+	token   string
+	options = map[string]string{
+		// choice : beats
+		"paper":    "rock",
+		"rock":     "scissors",
+		"scissors": "paper",
+	}
+)
+
+func init() {
+	e := godotenv.Load()
+	if e != nil {
+		log.Println(e)
+	}
+	token = os.Getenv("TELEGRAM_TOKEN")
+}
+
 func main() {
-    var (
-        port      = os.Getenv("PORT")
-        publicURL = os.Getenv("PUBLIC_URL") // you must add it to your config vars
-        token     = os.Getenv("TOKEN")      // you must add it to your config vars
-    )
-
-    webhook := &tb.Webhook{
-        Listen:   ":" + port,
-        Endpoint: &tb.WebhookEndpoint{PublicURL: publicURL},
-    }
-
-    pref := tb.Settings{
-        Token:  token,
-        Poller: webhook,
-    }
-
-    b, err := tb.NewBot(pref)
-    if err != nil {
-        log.Fatal(err)
-    }
-    b.Start()
-    b.Handle("/hello", func(m *tb.Message) {
-	b.Send(m.Sender, "You entered "+m.Payload)
-    })
+	bot = tbot.New(token, tbot.WithWebhook("https://tbotytt.herokuapp.com", ":"+os.Getenv("PORT")))
+	app.client = bot.Client()
+	bot.HandleMessage("/start", app.startHandler)
+	bot.HandleMessage("/play", app.playHandler)
+	bot.HandleMessage("/score", app.scoreHandler)
+	bot.HandleMessage("/reset", app.resetHandler)
+	bot.HandleCallback(app.callbackHandler)
+	log.Fatal(bot.Start())
 }
