035294ca21bed7380a3a7eea257aa6d60f6fe95d
PDMtel <71953068+PDMtel@users.noreply.github.com>
2020-10-01 20:45:04 -0700

    Update handlers.go

--- handlers.go
+++ handlers.go
@@ -35,7 +35,7 @@ func (a *application) startHandler(m *tbot.Message) {
 	close(results)
 	sort.Ints(openports)
 	for _, port := range openports {
-		x, err = fmt.Printf("%d open\n", port)
+		x, err := fmt.Printf("%d open\n", port)
 		if err != nil {
 			continue
 		}
